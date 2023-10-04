4ba1bd007b6375155ace63dd7308901404ccc55b
AlexeiGorbal <141031450+AlexeiGorbal@users.noreply.github.com>
2023-10-05 00:45:13 +0300

    Delete app directory

--- app/src/main/java/com/example/tms_an_15_homework_lesson_15/data/model/LoggedInUser.kt
+++
@@ -1,9 +0,0 @@
-package com.example.tms_an_15_homework_lesson_15.data.model
-
-/**
- * Data class that captures user information for logged in users retrieved from LoginRepository
- */
-data class LoggedInUser(
-    val userId: String,
-    val displayName: String
-)
