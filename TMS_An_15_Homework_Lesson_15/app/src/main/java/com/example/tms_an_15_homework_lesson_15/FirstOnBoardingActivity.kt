c2eb33cff9613afadb936e51fc02f4b5748912b4
AlexeiGorbal <141031450+AlexeiGorbal@users.noreply.github.com>
2023-10-05 00:46:42 +0300

    Add files via upload

---
+++ TMS_An_15_Homework_Lesson_15/app/src/main/java/com/example/tms_an_15_homework_lesson_15/FirstOnBoardingActivity.kt
@@ -0,0 +1,17 @@
+package com.example.tms_an_15_homework_lesson_15
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FirstOnBoardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        startActivity(Intent(this, SecondOnBoardingActivity::class.java))
+    }
+}
---
+++ TMS_An_15_Homework_Lesson_15/app/src/main/java/com/example/tms_an_15_homework_lesson_15/RegistrationActivity.kt
@@ -0,0 +1,17 @@
+package com.example.tms_an_15_homework_lesson_15
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        startActivity(Intent(this, SplashActivity::class.java))
+    }
+}
